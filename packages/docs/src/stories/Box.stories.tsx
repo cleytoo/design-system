4b2ec429502577adab0b61b647bb27d9c1c4fa18
cleyton vinicius <cleytonterceiro@outlook.com>
2023-01-14 16:15:08 -0300

    feat: Add core components

---
+++ packages/docs/src/stories/Box.stories.tsx
@@ -0,0 +1,17 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Box, BoxProps, Text } from '@ignite-ui/react'
+
+export default {
+  title: 'Surfaces/Box',
+  component: Box,
+  args: {
+    children: <Text>Testando o elementoBox</Text>,
+  },
+  argTypes: {
+    children: {
+      type: 'symbol',
+    },
+  },
+} as Meta<BoxProps>
+
+export const Primary: StoryObj<BoxProps> = {}
